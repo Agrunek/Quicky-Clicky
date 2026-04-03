39fbca116ecf7eb29c15b1aacdcbeb90685ac421
Adrian Antczak <adek24680@gmail.com>
2026-04-03 12:40:47 +0200

    Add non-keyboard block

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
 import Router from './Router';
 
 const App = () => {
-  return <Router />;
+  const [keyPressed, setKeyPressed] = useState(false);
+
+  useEffect(() => {
+    if (keyPressed) return;
+
+    const keydownHandler = () => setKeyPressed(true);
+    window.addEventListener('keydown', keydownHandler);
+
+    return () => window.removeEventListener('keydown', keydownHandler);
+  }, [keyPressed]);
+
+  if (keyPressed) return <Router />;
+
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <p className="text-2xl font-bold dark:text-white">Press any key to continue...</p>
+    </div>
+  );
 };
 
 export default App;
