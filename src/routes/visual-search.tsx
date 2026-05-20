d795bd1990e94b386dce57bb8def46c3d1c20238
Adrian Antczak <adek24680@gmail.com>
2026-05-20 13:35:45 +0200

    Refactor visual search

--- src/routes/visual-search.tsx
+++ src/routes/visual-search.tsx
@@ -3,6 +3,9 @@ import type { EvaluateReactionFunction } from '@/hooks/useGameState';
 import { useCallback, useLayoutEffect, useMemo, useState } from 'react';
 import { createFileRoute } from '@tanstack/react-router';
 
+import Card from '@/components/atoms/Card';
+import CenterWrapper from '@/components/atoms/CenterWrapper';
+import Text from '@/components/atoms/Text';
 import ResultView from '@/components/organisms/ResultView';
 import SetupView from '@/components/organisms/SetupView';
 import useGameState from '@/hooks/useGameState';
@@ -14,27 +17,24 @@ const VisualSearch = () => {
   const [alphaPair, setAlphaPair] = useState<[string, string[]]>(['', []]);
 
   const evaluateReactionFn: EvaluateReactionFunction = useCallback(
-    (time, confirmation) => {
-      return {
-        falseStart: false,
-        intentMatch: alphaPair[1].includes(alphaPair[0]),
-        isCorrect: confirmation === alphaPair[1].includes(alphaPair[0]),
-        reactionTimeMs: time,
-      };
-    },
+    (time, confirmation) => ({
+      falseStart: false,
+      intentMatch: alphaPair[1].includes(alphaPair[0]),
+      isCorrect: confirmation === alphaPair[1].includes(alphaPair[0]),
+      reactionTimeMs: time,
+    }),
     [alphaPair],
   );
 
   const { restartFn, setupFn, startFn, state } = useGameState(false, evaluateReactionFn);
 
+  const size = useMemo(() => state.setup.numberOfItems || 0, [state.setup.numberOfItems]);
+  const cols = useMemo(() => getRectangularGrid(size)[1], [size]);
+
   useLayoutEffect(() => {
     /* eslint-disable-next-line react-hooks/set-state-in-effect */
-    setAlphaPair(
-      getAlphaPair(state.results, state.setup.trialCount - state.currentTrial + 1, state.setup.numberOfItems || 0),
-    );
-  }, [state.setup, state.currentTrial, state.results]);
-
-  const [, cols] = useMemo(() => getRectangularGrid(state.setup.numberOfItems || 0), [state.setup]);
+    setAlphaPair(getAlphaPair(state.results, state.setup.trialCount - state.currentTrial + 1, size));
+  }, [state.setup, state.currentTrial, state.results, size]);
 
   if (state.status === 'prep') {
     return <SetupView includeKeyDeny includeNumberOfItems setup={state.setup} setupFn={setupFn} startFn={startFn} />;
@@ -42,34 +42,35 @@ const VisualSearch = () => {
 
   if (state.status === 'dead') {
     return (
-      <ResultView
-        name={`Visual Search (${state.setup.numberOfItems})`}
-        includeDecission
-        restartFn={restartFn}
-        results={state.results}
-      />
+      <ResultView name={`Visual Search (${size})`} includeDecission restartFn={restartFn} results={state.results} />
     );
   }
 
   return (
-    <>
-      <div className="flex min-h-screen w-full flex-col items-center justify-center">
-        <div className="flex items-center justify-around gap-6 rounded-2xl border border-black/25 bg-black/25 px-6 py-4 shadow-md backdrop-blur-xs dark:border-white/50 dark:bg-white/25">
-          <p className="mb-1 min-w-32 text-center text-4xl text-white">{alphaPair[0]}</p>
-          <div className="h-16 w-0.5 bg-white/50" />
-          <div style={{ gridTemplateColumns: `repeat(${cols}, 1fr)` }} className="grid min-w-32">
+    <CenterWrapper>
+      <Card className="flex min-w-80 items-center justify-between gap-4 p-4">
+        <Text variant="subheading" className="flex-1 text-center text-3xl!">
+          {alphaPair[0]}
+        </Text>
+        <div className="h-16 w-0.5 bg-white/50" />
+        <div className="flex flex-1 items-center justify-center">
+          <div style={{ gridTemplateColumns: `repeat(${cols}, 1fr)` }} className="grid">
             {alphaPair[1].map((alpha, idx) => (
-              <div key={idx} className="m-auto size-12 border border-white/50 bg-white/25">
-                {state.reactionReady && <p className="size-full text-center text-4xl text-white">{alpha}</p>}
+              <div key={idx} className="flex size-12 items-center justify-center border border-white/50 bg-white/25">
+                {state.reactionReady && (
+                  <Text variant="subheading" className="pb-0.5 text-3xl!">
+                    {alpha}
+                  </Text>
+                )}
               </div>
             ))}
           </div>
         </div>
-      </div>
-    </>
+      </Card>
+    </CenterWrapper>
   );
 };
 
 export const Route = createFileRoute('/visual-search')({
-  component: VisualSearch,
+  component: () => <VisualSearch />,
 });
