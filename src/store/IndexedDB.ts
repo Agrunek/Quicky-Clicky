2f326848fed82deaae4210facc11186989418116
Adrian Antczak <adek24680@gmail.com>
2026-05-22 00:36:52 +0200

    Add data stores

--- src/store/IndexedDB.ts
+++ src/store/IndexedDB.ts
@@ -1,6 +1,12 @@
 const DB_NAME = 'GAMES_HISTORY';
 const DB_VERSION = 1;
 
+const SIMPLE_REACTION_STORE = 'SimpleReaction';
+const PHYSICAL_MATCHING_STORE = 'PhysicalMatching';
+const NAME_MATCHING_STORE = 'NameMatching';
+const CLASS_MATCHING_STORE = 'ClassMatching';
+const VISUAL_SEARCH_STORE = 'VisualSearch';
+
 const DB: IDBDatabase | null = await new Promise((resolve) => {
   const DBOpenRequest = indexedDB.open(DB_NAME, DB_VERSION);
 
@@ -15,7 +21,27 @@ const DB: IDBDatabase | null = await new Promise((resolve) => {
   };
 
   DBOpenRequest.onupgradeneeded = () => {
-    // Create store for the DB
+    const config = DBOpenRequest.result;
+
+    if (!config.objectStoreNames.contains(SIMPLE_REACTION_STORE)) {
+      config.createObjectStore(SIMPLE_REACTION_STORE, { autoIncrement: true });
+    }
+
+    if (!config.objectStoreNames.contains(PHYSICAL_MATCHING_STORE)) {
+      config.createObjectStore(PHYSICAL_MATCHING_STORE, { autoIncrement: true });
+    }
+
+    if (!config.objectStoreNames.contains(NAME_MATCHING_STORE)) {
+      config.createObjectStore(NAME_MATCHING_STORE, { autoIncrement: true });
+    }
+
+    if (!config.objectStoreNames.contains(CLASS_MATCHING_STORE)) {
+      config.createObjectStore(CLASS_MATCHING_STORE, { autoIncrement: true });
+    }
+
+    if (!config.objectStoreNames.contains(VISUAL_SEARCH_STORE)) {
+      config.createObjectStore(VISUAL_SEARCH_STORE, { autoIncrement: true });
+    }
 
     console.info('New version of database has been created.');
   };
